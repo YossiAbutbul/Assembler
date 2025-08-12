763b245562ec3543e5a9f3f06bf5825adbd9c7c7
YossiAbutbul <abyossi22@gmail.com>
2025-08-12 10:10:04 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,8 +1,21 @@
 /**
-* @file main.c
-* @author Yossi Abutbul
-* ToDo: add propper documentation.
-*/
+ * @file main.c
+ * @author Yossi Abutbul
+ * @brief Main entry point for the assembler program.
+ *
+ * This program processes assembly language source file (.as) and generates
+ * output files. It handles multiple input files and processes
+ * each one independently.
+ *
+ * Usage: todo
+ *
+ * For each input "filename", the program:
+ * 1. Reads filename.as (source file).
+ * 2. Generates filename.am (macro expanded file).
+ * 3. Generates filename.ob (object file in base-4 encoding).
+ * 4. Generates filename.ent (entry symbols, if any).
+ * 5. Generates filename.ext (external references, if any).
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,13 +27,16 @@ int main(int argc, char *argv[])
 {
     int i;
     ExitCode result;
+    BOOL has_errors = FALSE;
 
+    /* Check minimum number of arguments */
     if (argc < MIN_ARGC)
     {
         printf("Usage: %s file1 [file2 ...]\n", argv[0]);
         return EXIT_GENERAL_ERROR;
     }
 
+    /* Process each input file */
     for (i = 1; i < argc; i++)
     {
         printf("Assembling file: %s.as\n", argv[i]);
@@ -28,10 +44,17 @@ int main(int argc, char *argv[])
 
         if (result != EXIT_SUCCESS_CODE)
         {
-            report_error(result, argv[i]);
-            return result;
+            has_errors = TRUE;
+            printf("Continuing with next file...\n");
         }
+        else
+            printf("Successfully assembled %s.as\n", argv[i]);
+
+        /* Add blank line between files for readability */
+        if (i < argc - 1)
+            printf("\n");
     }
 
-    return EXIT_SUCCESS_CODE;
+    /* Return success if all files succeeded, error if any failed */
+    return has_errors ? EXIT_GENERAL_ERROR : EXIT_SUCCESS_CODE;
 }
